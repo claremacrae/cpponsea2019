7a07e2f9128ba806154f70e1f8f2e2ccac1dfb1d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-01-12 17:17:04 +0000

    Add some explanatory comments

--- demo_approvals_and_catch2/Test01.cpp
+++ demo_approvals_and_catch2/Test01.cpp
@@ -6,3 +6,8 @@ TEST_CASE( "Sums are calculated" )
     REQUIRE( 1 + 1 == 2 );
     REQUIRE( 1 + 2 == 3 );
 }
+
+/*
+ * Things to note here:
+ * This is an ordinary Catch2 test - there is nothing Approvals-related here
+ */
