ead3d56224fc289252cbcc60109c4868222e546d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-01-12 22:18:35 +0000

    88%

--- gilded_rose_refactoring_kata/GildedRoseApprovals.cpp
+++ gilded_rose_refactoring_kata/GildedRoseApprovals.cpp
@@ -85,7 +85,8 @@ TEST(GildedRoseApprovalTests, VerifyCombinations)
 
 TEST(GildedRoseApprovalTests, VerifyMoreCombinations)
 {
-    std::vector<string> names { "Foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert" };
+    std::vector<string> names { "Foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert",
+                                "Sulfuras, Hand of Ragnaros"};
     std::vector<int> sellIns { 0 };
     std::vector<int> qualities { 0, 1, 2 };
 
