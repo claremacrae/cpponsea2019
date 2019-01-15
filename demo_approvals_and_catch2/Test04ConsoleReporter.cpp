bef3d3d2911c9440c306ebf2a9b02e859e09d78d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-01-15 06:29:03 +0000

    Add support for console diff tool on Linux and Mac

--- demo_approvals_and_catch2/Test04ConsoleReporter.cpp
+++ demo_approvals_and_catch2/Test04ConsoleReporter.cpp
@@ -44,8 +44,14 @@ namespace
 
 TEST_CASE("UseConsoleReporter")
 {
-    // On Windows, SystemLauncher does not yet know how to use the "where" command to find
-    // the location of a program that is in the PATH, so we have to give the full
-    // path to fc.exe here:
-    Approvals::verify("Some\nMulti-line\noutput", ConsoleDiffReporter("C:\\Windows\\System32\\fc.exe"));
+    FirstWorkingReporter diff_reporter(
+        {
+            // On Windows, SystemLauncher does not yet know how to use the "where" command to find
+            // the location of a program that is in the PATH, so we have to give the full
+            // path to fc.exe here:
+            new ConsoleDiffReporter("C:\\Windows\\System32\\fc.exe"),
+            // On Linux and Mac, this one will be used:
+            new ConsoleDiffReporter("diff")
+        });
+    Approvals::verify("Some\nMulti-line\noutput", diff_reporter);
 }
