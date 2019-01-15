7ba46dd8232117a6df5f1dc42f1ae7dd9286e9e9
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-01-15 06:41:29 +0000

    Show custom and console reporters in google test

---
+++ demo_approvals_and_googletest/Test03CustomReporters.cpp
@@ -0,0 +1,12 @@
+#include "ApprovalTests.hpp"
+#include <gtest/gtest.h>
+
+TEST(Test03, UseSpecificReporter)
+{
+    Approvals::verify("Some\nMulti-line\noutput", Windows::AraxisMergeReporter());
+}
+
+TEST(Test03, UseCustomReporter)
+{
+    Approvals::verify("Some\nMulti-line\noutput", GenericDiffReporter("C:\\Program Files\\TortoiseHg\\lib\\kdiff3.exe"));
+}
