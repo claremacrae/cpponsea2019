db619519543fc5df746f1b67aa1b6962cd8fae67
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-01-25 15:11:47 +0000

    Now testing the actual calculation result (new quality)

--- gilded_rose_refactoring_kata/GildedRoseTest.cpp
+++ gilded_rose_refactoring_kata/GildedRoseTest.cpp
@@ -17,3 +17,21 @@ TEST(GildedRoseTest, Foo)
  * - 52% coverage of GildedRose.cpp
  * - The actual calculation result (new quality) is not tested
  */
+
+TEST(GildedRoseTest, TestUpdateQuality)
+{
+    vector<Item> items;
+    items.push_back(Item("Foo", 1, 1));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ("Foo", app.items[0].name);
+    EXPECT_EQ(0, app.items[0].sellIn);
+    EXPECT_EQ(0, app.items[0].quality);
+}
+
+/*
+ * Things to note here:
+ * 
+ * - 52% still coverage of GildedRose.cpp
+ * - Now testing the actual calculation result (new quality)
+ */
