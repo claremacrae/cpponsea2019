3775bf56a018dbd82347078a4ad28be7e0985fd6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-02-02 09:59:05 +0000

    Show use of CombinationApprovals::verifyAllCombinations() in Google Test framework

---
+++ demo_approvals_and_googletest/Test06VerifyCombinations.cpp
@@ -0,0 +1,74 @@
+#include "ApprovalTests.hpp"
+#include <gtest/gtest.h>
+
+namespace
+{
+    std::string concatenateStringAndInt(std::string s, int i)
+    {
+        return s + " " + std::to_string(i);
+    }
+}
+
+TEST(Test06VerifyCombinations, verifyAllCombinationsWithFunction)
+{
+    const std::vector<std::string> strings{"hello", "world"};
+    const std::vector<int> numbers{1, 2, 3};
+
+    CombinationApprovals::verifyAllCombinations<
+        std::vector<std::string>,       // The type of element in our first input container
+        std::vector<int>,               // The type of element in our second input container
+        std::string>(                   // The return type from testing one combination of inputs
+            concatenateStringAndInt,    // Function that one combination of inputs, and returns the result to be approved
+            strings,                    // The first input container
+            numbers);                   // The second input container
+}
+/*
+ * Things to note here:
+ * 1. We are testing the behaviour of function concatenateStringAndInt(), 
+ *    with a range of input values
+ * 2. By adding more values to the input containers, we can very quickly get
+ *    good test coverage.
+ * 3. Approvals allows up to 9 different containers of inputs!
+ * 4. Here, the approved file will contain:
+
+(hello, 1) => hello 1
+(hello, 2) => hello 2
+(hello, 3) => hello 3
+(world, 1) => world 1
+(world, 2) => world 2
+(world, 3) => world 3
+
+ * 5. We an also supply a Reporter as an extra argument.
+ */
+
+
+TEST(Test06VerifyCombinations, verifyAllCombinationsWithLambda)
+{
+    std::vector<std::string> strings{"hello", "world"};
+    std::vector<int> numbers{1, 2, 3};
+    CombinationApprovals::verifyAllCombinations<
+        std::vector<std::string>,       // The type of element in our first input container
+        std::vector<int>,               // The type of element in our second input container
+        std::string>(                   // The return type from testing one combination of inputs
+            // Lambda that acts on one combination of inputs, and returns the result to be approved:
+            [](std::string s, int i) { return s + " " + std::to_string(i); },
+            strings,                    // The first input container
+            numbers);                   // The second input container
+}
+/*
+ * Things to note here:
+ * 1. We are now using a lambda to test concatenating two values, with a range of input values
+ * 2. By adding more values to the input containers, we can very quickly get
+ *    good test coverage.
+ * 3. Approvals allows up to 9 different containers of inputs!
+ * 4. Here, the approved file will contain:
+
+(hello, 1) => hello 1
+(hello, 2) => hello 2
+(hello, 3) => hello 3
+(world, 1) => world 1
+(world, 2) => world 2
+(world, 3) => world 3
+
+ * 5. We an also supply a Reporter as an extra argument.
+ */
